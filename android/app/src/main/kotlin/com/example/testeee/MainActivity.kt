8b5ebf6b8ff0f351865b48c72d1279f97bbabfc4
Faimo-M <faimom258@gmail.com>
2026-03-18 09:39:03 +0200

    Criando uma app de gestao de tarefas, primeira interacao com flutter

---
+++ android/app/src/main/kotlin/com/example/testeee/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.testeee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/task_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen  extends StatefulWidget{
+  const TasksScreen({super.key});
+
+  @override
+State<TasksScreen> createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  List<String> tarefas = [
+    "Estudar flutter",
+    "Fazer exercicios",
+    "Preparar aula",
+    "JoyBoy Is the best",
+  ];
+
+  final TextEditingController tarefaController = TextEditingController();
+  @override
+   Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: const Text("App de Tarefas Joy")),
+     body: Padding(
+         padding: const EdgeInsets.all(16.0),
+     child: Column(
+       children:[TextField(
+         controller: tarefaController,
+         decoration: const InputDecoration(
+           labelText: "Nova Tarefa",
+           border: OutlineInputBorder(),
+         ),
+       ),
+       const SizedBox(height: 12),
+         ElevatedButton(
+             onPressed:() {
+               if (tarefaController.text.isNotEmpty){
+                 setState(() {
+                   tarefas.add(tarefaController.text);
+                 });
+                 tarefaController.clear();
+               }
+             },
+         child: const Text("Adicionar"),),
+       const SizedBox(height: 20),
+       Expanded(
+           child: ListView.builder(
+               itemCount: tarefas.length,
+             itemBuilder: (context, index){
+                 return ListTile(title: Text(tarefas[index]));
+             },
+           ),
+       ),
+       ],
+     ),
+     ),
+    );
+  }
+}
